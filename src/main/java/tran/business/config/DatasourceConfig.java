45e4d537f8a5183c572197795039cc9c4444fb43
raphtalia <59540801+raphtalia@users.noreply.github.com>
2022-07-27 18:35:24 -0500

    Moved configs to config package

--- src/main/java/tran/business/config/DatasourceConfig.java
+++ src/main/java/tran/business/config/DatasourceConfig.java
@@ -1,3 +1,5 @@
+package tran.business.config;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
